9c5b9dd6f4112d30a0ccd36b166b0bf81cd0247b
Hattou Faïssal <faissal.hattou@gmail.com>
2018-01-22 14:23:24 +0100

    add home page component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h4>Home page en construction... Cliquez sur 'Parcourir' dans la barre de navigation pour commencer :)</h4>
+        <h4>Page d'accueil en construction... Cliquez sur 'Parcourir' dans la barre de navigation pour commencer :)</h4>
       </div>
     )
   }
