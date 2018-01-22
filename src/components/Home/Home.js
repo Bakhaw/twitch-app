cfcfdc1f233a51c9623fc636d49c11ffd48d9e15
Hattou Faïssal <faissal.hattou@gmail.com>
2018-01-22 14:20:56 +0100

    add Home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './Home.scss';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h4>Home page en construction... Cliquez sur 'Parcourir' dans la barre de navigation pour commencer :)</h4>
+      </div>
+    )
+  }
+}
+export default Home;
