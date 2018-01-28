4fe6edca351506b612b3f849ee6357d65e05598f
Hattou Faïssal <faissal.hattou@gmail.com>
2018-01-28 05:20:06 +0100

    add button to go back to /directory from the streams

--- src/components/Streams/Header.js
+++ src/components/Streams/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { Link } from 'react-router-dom';
+
 import { connect } from "react-redux";
 import { fetchGames } from "../../redux/actions/fetchGames";
 
@@ -11,21 +13,27 @@ class Header extends Component {
   }
 
   render() {
+
     return (
       <div className="headerContainer">
         {/* Game Title */}
         {this.props.games.games.map((game, index) => {
+          
+          const backgroundImage = `linear-gradient(rgb(0, 0, 0) 0, rgba(0, 0, 0, 0.6) 0), url(${game.box_art_url.slice(0, -21)}.jpg)`
+          
           return (
             <div
               key={index}
               className="headerContent"
-              style={{
-                backgroundImage: `url(${game.box_art_url.slice(0, -21)}.jpg)`,
-              }}
-            >
+              style={{ backgroundImage }}>
               <div>
                 <img src={`${game.box_art_url.slice(0, -21)}.jpg)`} alt={`${game.name} cover image`}/>
-                <h2>{game.name}</h2>
+                <div>
+                  <Link to="/directory">
+                    <h4><i className="ion-chevron-left"></i> Parcourir</h4>
+                  </Link>
+                  <h2>{game.name}</h2>
+                </div>
               </div>                           
             </div>
           );
